8f26819c7758dc5a009caa2103c23784f1e73f26
AryaGosuDev <karrat@shucklak.com>
2017-09-28 03:14:57 -0700

    update commit

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <map>
 #include <set>
 #include <sstream>
+#include <memory>
 
 using std::endl;
 using std::cout;
@@ -25,57 +26,133 @@ using std::for_each;
 using std::vector;
 using std::list;
 
+class StrBlobPtr ;
+
 class StrBlob {
 
 public:
 	StrBlob() {};
 	StrBlob(std::initializer_list<string> li);
+	
+	friend class StrBlobPtr ;
+	
+	StrBlobPtr begin() ;
+	StrBlobPtr end();
 
 	string back() const ;
 	string front() const ;
-
+	
 	void print () const  {
 		for_each ( data->begin(), data->end(), [](string & vecString) { cout << vecString << endl ;});
-
 	}
 
 private:
-
 	std::shared_ptr<vector<string>> data ;
 };
 
-StrBlob::StrBlob(std::initializer_list<string> li) : data(std::make_shared<vector<string>>(li)){
+class StrBlobPtr {
+	
+	public :
+		StrBlobPtr () : current(0)  {} ;
+		StrBlobPtr ( StrBlob &a, size_t _curr = 0 ) : wptr ( a.data) , current ( _curr) {	
+		}
+		std::string & deref () const ;
+		StrBlobPtr & incr() ;
+		
+		size_t current ;
+		
+	private :
+	
+		std::shared_ptr<std::vector<string>>
+		check ( size_t i) const ;
+	
+		std::weak_ptr<vector<std::string>> wptr ;
+		
+		
+};
 
+bool operator!=(const StrBlobPtr & lhs, const StrBlobPtr & rhs){
+			return lhs.current != rhs.current;
+		}
+
+StrBlob::StrBlob(std::initializer_list<string> li) : data(std::make_shared<vector<string>>(li)){
 }
 
 string StrBlob::back() const {
 	return data->back();
-
 }
-
 string StrBlob::front() const {
 	return data->front();
 }
+
+StrBlobPtr StrBlob::begin () {
+	return StrBlobPtr ( *this);
+}
+
+StrBlobPtr StrBlob::end () {
+	return StrBlobPtr ( *this, data->size() );
+}
+
+std::shared_ptr<std::vector<string>> StrBlobPtr::check( size_t i ) const {
+	auto ret = wptr.lock() ;
+	if ( !ret )
+		throw std::runtime_error ( "unbound StrBlob");
+	if ( i >= ret->size() )
+		throw std::out_of_range ( "Out of range StrBlob");
+	return ret ;
+}
+
+std::string &  StrBlobPtr::deref() const {
+	auto ptr = check ( current);
+	return (*ptr)[current];
+}
+
+StrBlobPtr & StrBlobPtr::incr() {
+	
+	check ( current  ) ;
+	++current ;
+	return *this ;
+} 
 
 int main ( void ) {
 
   try {
 
-  	StrBlob b1 ;
-  	{
-  		StrBlob b2 = { "a", "b", "c"} ;
-  		b1 = b2 ;
+  	StrBlob b1 { "a", "b", "c"} ;
+	
+	for ( StrBlobPtr beg = b1.begin() ; beg != b1.end() ; beg.incr()) {
+		//cout << beg.deref() << endl;
+		
+	}
+	
+	string strInput ;
+	
+	size_t charArraySize = 20 ;
+	
+	char * inputCharStringArray = new char[charArraySize] ;
+	char * tempCharArray ;
+	
+	int currentArrayIndx = 0;
+	
+	while ( std::cin >> strInput) {
+		
+		if ( strInput.size() + currentArrayIndx > charArraySize ) {
+			//we need a bigger array to store the new string
+			tempCharArray = inputCharStringArray ;
+			charArraySize *= 2 ;
+			inputCharStringArray = new char[charArraySize] ;
+			cout << "Deleting old string of size" << charArraySize / 2  << endl ;
+			delete [] tempCharArray ;
+		}
+		
+		for ( int i = 0 ; i < strInput.size() ; i++  ) 
+			inputCharStringArray[currentArrayIndx++] = strInput[i] ;
 
-
-
-  	}
-
-  	cout << b1.back();
-
-  	b1.print();
-
-  	//for_each ( )
-
+	}
+	
+	
+	
+	//cout << "test" ;
 	}
 	catch ( std::exception ex) {
 		cout << "Error : " << ex.what() << endl ;
