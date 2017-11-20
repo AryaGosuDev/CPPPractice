caf7162e1a72367e04ddfea2ce02653289ba0d77
AryaGosuDev <karrat.perjia@shucklak.com>
2017-11-20 12:31:39 -0800

    14.1 end templated my_shared_ptr class

--- main.cpp
+++ main.cpp
@@ -29,43 +29,137 @@ using std::for_each;
 using std::vector;
 using std::list;
 
-template < typename T > bool compare ( const T a, const T b ) {
-  
-  return a > b ;
 
-}
+class DebugDelete {
+  public :
+  DebugDelete() = default ;
 
-template < typename T , typename U> int findThis ( T first, T last, U valToFind ) {
-  
-  //for_each ( p.begin(), p.end(), [valToFind](T a ) { cout << a << endl; if ( a == valToFind) return 1 ;}  );
+  template < typename T > inline bool operator() ( T * p ) {
+   cout << "Deleting pointer" <<  endl ;
+   delete p ;
+   return true ;
 
-  for (  ; first != last; ++ first ) {
-
-    if ( (*first) == valToFind ) return 1 ;
   }
-  return 0;
-}
 
-template <typename T, unsigned M> int print ( T (&p)[M]) {
 
-  for ( int i = 0; i < M ; i++) cout << p[i] << endl ;
+};
+
+
+template < typename T, typename U = std::default_delete<T> >
+class my_shared_ptr {
+  public :
+    
+    my_shared_ptr<T, U> () : refcount ( new int(1)), thisObj(0),  del ( new std::default_delete<T>()) {} 
+    my_shared_ptr<T, U> ( T & t) : refcount ( new int(1)), thisObj(&t), del ( new std::default_delete<T>()) {} 
+    my_shared_ptr<T, U> ( T & t,  U & u) : refcount ( new int(1)), thisObj(&t), del ( &u) {} 
+    my_shared_ptr<T, U> ( T & t,  U * u) : refcount ( new int(1)), thisObj(&t), del ( u) {} 
+    //my_shared_ptr<T, U> ( T & t,  U && u) : refcount ( new int(1)), thisObj(&t), del ( &u) {}
+    my_shared_ptr<T, U> ( T * t,  U * u, int * i ) : thisObj ( t) , del ( u), refcount(i) {}
+
+    my_shared_ptr<T, U> ( my_shared_ptr && msp ) : refcount ( msp.refcount) , thisObj(msp.thisObj), del ( msp.del) {
+      cout << "Here" ;
+      msp.refcount = NULL ;
+      msp.thisObj = NULL ;
+      msp.del = NULL;
+    } 
+
+    ~my_shared_ptr() {
+        refcount[0] -- ;
+      if ( refcount[0] == 0 && thisObj != NULL )
+        (*del) (thisObj);
+    }
+
+    int returnRefCount() const { return refcount[0];}
+
+    void incrCount ( ) { this->refcount[0]++;}
+
+    static my_shared_ptr<T,U> my_make_shared ( my_shared_ptr & msp) {
+
+      //cout << "Here" ;
+
+      msp.incrCount();
+
+      return my_shared_ptr<T, U> ( msp.thisObj, msp.del, msp.refcount);
+    }
+
+  private :
+
+    int * refcount ;
+
+    T * thisObj;
+
+    U * del ;
+};
+
+
+template < typename T >
+class Blob {
+
+  public :
+
+  typedef typename vector<T>::size_type size_type ;
+
+  Blob<T>();
+  Blob<T>(std::initializer_list<T> il );
   
-  return 1;
-}
+  size_type size() const { return data->size();}
+  bool empty() const { return data->emtpy();}
+
+  void push_back( const T &t) { data->push_back(t);}
+  void pop_back();
+
+  T & front();
+  T & back();
+
+  private :
+
+    //std::shared_ptr<vector<T>> data;
+
+    my_shared_ptr<std::vector<T>, DebugDelete> data;
+
+    void check ( size_type i, const T & msg) const ;
+
+};
+
+template < typename T >
+class BlobPtr {
+
+  public :
+
+  BlobPtr<T>() : curr(0) {}
+  BlobPtr<T>( Blob<T> &a, size_t sz = 0 ) :
+    wptr( a.data ) , curr ( sz ) {}
+
+  T & deref() const ;
+  BlobPtr<T> & incr ();
+
+  private :
+
+  std::shared_ptr<std::vector<T>>
+    check( size_t, const T &) const;
+
+  std::weak_ptr<vector<T>> wptr ;
+  size_t curr;
+};
 
 
 int main ( void ) {
 
   try {
 
-    //cout << compare ( 1 , 2 ) << endl ;
+    int * p = new int(5);
+    DebugDelete dd;
+    dd (p);
 
-    string findS = "g" ;
+    int * ip = new int(5);
 
-    vector<string> thisVec = {"fff", "g", "h", "j" };
-    //cout << findThis ( thisVec.begin(), thisVec.end(), findS) << endl;
-    int p{4444, 3, 4 , 4} ;
-    print(p );
+    my_shared_ptr<int, DebugDelete> p_shared( ip[0], new DebugDelete());
+
+    cout << p_shared.returnRefCount() << endl;
+
+    my_shared_ptr<int, DebugDelete> newShared ( my_shared_ptr<int, DebugDelete>::my_make_shared ( p_shared ));
+
+    cout << newShared.returnRefCount() << endl;
 
     return 0;
 
